2f0f39a70e14d1b505152abd8e07bce21a308166
[Sebasplaza] <[sebastianmultimedial@gmail.com]>
2022-08-04 23:36:30 -0300

    se creo el router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ArrowRight, Cart, CartDash } from 'react-bootstrap-icons';
 import CartWidget from './CartWidget';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
+    
+    
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
     <div className="container">
-      <a className="navbar-brand text-light" href="#">Navbar</a>
+      <NavLink className="navbar-brand text-light" to="/">SPYKE STRORE</NavLink>
       
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -15,11 +18,9 @@ const Navbar = () => {
       <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
         <ul className="navbar-nav ">
           <li className="nav-item">
-            <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+          <NavLink className="navbar-brand text-light" to="/">Home</NavLink>
           </li>
-          <li className="nav-item">
-            <a className="nav-link text-light" href="#">Comics</a>
-          </li>
+         
           <li className="nav-item">
             <a className="nav-link text-light" href="#">Mangas</a>
           </li>
@@ -29,6 +30,8 @@ const Navbar = () => {
      <CartWidget/>
     </div>
   </nav>
+ 
+ 
   )
 }
 
